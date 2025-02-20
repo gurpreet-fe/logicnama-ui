9f9d22464d11bad6c7f84d7b722ba23aae65aae6
Gurpreet Singh <gur.singh2022@gmail.com>
2025-02-20 21:59:56 +0000

    feat: design updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Logical Fallacies For Sikhs";
+export const SITE_TITLE = "Logicnama.com";
 export const SITE_DESCRIPTION =
   "A List Of Logical Fallacies and Sikh relevant examples";
