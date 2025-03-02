cb7cc2a3afabe09109b106c4df52fb5e97e0c814
Gurpreet Singh <gur.singh2022@gmail.com>
2025-03-02 13:15:37 +0000

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://logicnama.com",
   integrations: [
     mdx(),
     sitemap(),
