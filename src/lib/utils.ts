32e76eadf1d25a8ed00e25f4504c2807fa332947
Gurpreet Singh <gur.singh2022@gmail.com>
2025-03-02 15:53:59 +0000

    feat: updates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function alphabeticalSort(a: any, b: any) {
+  if (a.data.title < b.data.title) {
+    return -1;
+  }
+  if (a.data.title > b.data.title) {
+    return 1;
+  }
+  return 0;
+}
