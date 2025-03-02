32facad4a3592ac0ea29b6d20e6d531c7745f739
Gurpreet Singh <gur.singh2022@gmail.com>
2025-03-02 16:28:43 +0000

    feat: partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://logicnama.com",
@@ -16,6 +18,11 @@ export default defineConfig({
     react(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
   experimental: {
